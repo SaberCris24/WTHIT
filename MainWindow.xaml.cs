78420103bd0237ee9e0b2a85545b70f2dac1e914
SaberCris24 <crisdax2442@gmail.com>
2025-05-13 20:09:48 -0500

    Boton about y github

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Documents;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
@@ -57,10 +59,9 @@ namespace Plantilla
 
         private void SearchBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
         {
-
             if (args.QueryText != string.Empty)
             {
-
+                // Lógica para procesar la consulta
             }
         }
 
@@ -81,6 +82,61 @@ namespace Plantilla
         private void SearchBox_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
         {
             sender.Text = args.SelectedItem.ToString();
+        }
+
+        private void AboutButton_Click(object sender, RoutedEventArgs e)
+        {
+            StackPanel contentPanel = new StackPanel();
+
+            TextBlock infoText = new TextBlock
+            {
+                Text = "WTHIT\nVersion 1.0\n\nDeveloped by: Geriberto\n",
+                TextWrapping = TextWrapping.Wrap
+            };
+            contentPanel.Children.Add(infoText);
+
+            TextBlock linkText = new TextBlock
+            {
+                TextWrapping = TextWrapping.Wrap
+            };
+
+            Hyperlink hyperlink = new Hyperlink
+            {
+                NavigateUri = new Uri("https://github.com/SaberCris24/WTHIT")
+            };
+
+            Run linkRun = new Run
+            {
+                Text = "GitHub: https://github.com/SaberCris24/WTHIT"
+            };
+
+            hyperlink.Inlines.Add(linkRun);
+            
+            hyperlink.Click += Hyperlink_Click;
+
+            linkText.Inlines.Add(hyperlink);
+
+            contentPanel.Children.Add(linkText);
+
+            ContentDialog aboutDialog = new ContentDialog
+            {
+                Title = "About",
+                Content = contentPanel,
+                CloseButtonText = "Close",
+                XamlRoot = this.Content.XamlRoot
+            };
+
+            _ = aboutDialog.ShowAsync();
+        }
+
+        // Método para el click del hyperlink
+        private async void Hyperlink_Click(Hyperlink sender, HyperlinkClickEventArgs args)
+        {
+
+            if (sender.NavigateUri != null)
+            {
+               await Windows.System.Launcher.LaunchUriAsync(sender.NavigateUri);
+            }
         }
     }
 }

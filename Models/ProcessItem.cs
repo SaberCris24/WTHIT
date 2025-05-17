a6ee12263cc85ab608ab240a956472e6f9a634af
SaberCris24 <crisdax2442@gmail.com>
2025-05-17 03:46:10 -0500

    Automatic Process Relate And UI Changes

--- Models/ProcessItem.cs
+++ Models/ProcessItem.cs
@@ -1,11 +1,47 @@
 namespace Plantilla.Models
 {
-    public class ProcessItem
+    public class ProcessItem : System.ComponentModel.INotifyPropertyChanged
     {
+        private bool _isSelected;
+        private string _virusStatus;
+
         public string ProcessName { get; set; }
         public int ProcessId { get; set; }
         public string ApplicationRelated { get; set; }
-        public string VirusStatus { get; set; }
+        
+        public string VirusStatus
+        {
+            get => _virusStatus;
+            set
+            {
+                if (_virusStatus != value)
+                {
+                    _virusStatus = value;
+                    OnPropertyChanged(nameof(VirusStatus));
+                }
+            }
+        }
+        
         public string Information { get; set; }
+        
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set
+            {
+                if (_isSelected != value)
+                {
+                    _isSelected = value;
+                    OnPropertyChanged(nameof(IsSelected));
+                }
+            }
+        }
+
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+        }
     }
 }
